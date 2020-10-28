416ea10108a320c47449aab52ddb19d9fa1b1246
Robert De la Rosa <rdelarosa39@gmail.com>
2020-10-28 09:31:31 -0500

    Add: limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT * FROM employees
+WHERE birth_date LIKE '%12-25%'
+  AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5;
+
+-- limit and offset create a pagination
+SELECT * FROM employees
+WHERE birth_date LIKE '%12-25%'
+  AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
