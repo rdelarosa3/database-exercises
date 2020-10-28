72f986fb766211fafb2a430e8c3a752b827a2ea8
Robert De la Rosa <rdelarosa39@gmail.com>
2020-10-28 09:53:16 -0500

    Add: Functions exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya')
+ORDER BY last_name,first_name;
+
+SELECT CONCAT(first_name,' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E'
+ORDER BY emp_no DESC ;;
+
+SELECT CONCAT(first_name,' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT DATEDIFF(CURDATE(),hire_date)
+FROM employees
+WHERE birth_date LIKE '%12-25%'
+  AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC;
